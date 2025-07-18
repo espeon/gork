310a825558c6c87d412d4aa6f1a3ee3eb92c9e7d
natalie <22222885+espeon@users.noreply.github.com>
2025-07-18 18:21:11 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -152,7 +152,7 @@ impl LexiconIngestor for MyCoolIngestor {
             let riposte =
                 serde_json::from_value::<atrium_api::app::bsky::feed::post::RecordData>(record)?;
 
-            if !(riposte.text.starts_with("@gork")
+            if !(riposte.text.starts_with("@gork.bluesky.bot")
                 && (riposte.text.contains("is this")
                     || riposte.text.contains("am i")
                     || riposte.text.contains("do you")))
